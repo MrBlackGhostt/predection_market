236f6ef68d9097fc971c60e3b0a7320bb8dc75df
MrBlackGhostt <hemantkumar4213@icloud.com>
2026-02-07 22:22:29 +0530

    error added

--- programs/predection/src/errors.rs
+++ programs/predection/src/errors.rs
@@ -4,4 +4,16 @@ use anchor_lang::prelude::*;
 pub enum Errors {
     #[msg("Error")]
     ErrorCheck,
+    #[msg("The amount you enter is invalid")]
+    ErrorInvalidAmount,
+    #[msg("The vault is invalid")]
+    InvalidVault,
+    #[msg("The mint is invalid for the market")]
+    InvalidMint,
+    #[msg("The market creator is invalid")]
+    InvalidMarketAuthority,
+    #[msg("Market is not open for trading")]
+    InvalidMarketStatus,
+    #[msg("Market already finished")]
+    MarketFinished,
 }
